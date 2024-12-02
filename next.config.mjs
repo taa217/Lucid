a29e06310fddb8d6ad2ebf46ef0c85a563aaeb70
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2024-12-02 13:33:32 +0200

    Update next.config.mjs
    falsing canvas

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config) {
+      config.resolve.fallback = {
+        canvas: false, // Ignore the canvas module for the build
+      };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
