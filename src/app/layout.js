b679c6d9a18e2271cb38562340cb0627875beb0d
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2024-12-06 15:17:12 +0200

    Update layout.js
    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
+              function gtag(){dataLayer.push(arguments)}
               gtag('js', new Date());
               gtag('config', 'G-W0Q458T805');
             `,
