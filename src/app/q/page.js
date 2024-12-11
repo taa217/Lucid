69e2ff31cbd9473c87e752d7def583a487fe3911
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2024-12-11 17:27:52 +0200

    Update page.js
    reduce duration

--- src/app/q/page.js
+++ src/app/q/page.js
@@ -388,7 +388,7 @@ export default function Reader() {
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 1, duration: 1 }}
+        transition={{ delay: 0, duration: 3 }}
         style={{
           position: 'absolute',
           bottom: 30,
