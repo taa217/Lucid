563491491a1c9df22bcb1300076527eb78a43f45
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2024-12-15 01:07:08 +0200

    Update page.js
    add x link

--- src/app/page.js
+++ src/app/page.js
@@ -334,7 +334,7 @@ export default function Home() {
             <IconButton href="https://www.facebook.com/lucidai.company" target="_blank" color="inherit">
               <Facebook fontSize="large" />
             </IconButton>
-            <IconButton href="#" target="_blank" color="inherit">
+            <IconButton href="https://x.com/lucid_startup" target="_blank" color="inherit">
               <X fontSize="large" />
             </IconButton>
             <IconButton href="https://www.instagram.com/lucidai_company" target="_blank" color="inherit">
