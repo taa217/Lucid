ddc6623198094583e520abad59d72666e6244d38
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2024-12-15 13:39:41 +0200

    Update layout.js
    add viewport

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ const geistMono = localFont({
 export const metadata = {
   title: "Lucid",
   description: "Enhanced learning with AI. ",
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
 
 export default function RootLayout({ children }) {
