b99eb26c2264d362b193973dc278a6de9cb4506d
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2024-12-16 01:52:23 +0200

    Update GlowingQ.jsx
    width

--- components/GlowingQ.jsx
+++ components/GlowingQ.jsx
@@ -7,7 +7,7 @@ const GlowingQSVG = ({width,height}) => {
       <svg
         viewBox="0 0 100 100"
         xmlns="http://www.w3.org/2000/svg"
-        style={{ width: '100%', height: '100%' }}
+        style={{ width: '90%', height: '100%' }}
       >
         {/* Gradient Definition */}
         <defs>
